0a38bb28857ee265c39f05e112db4e25f2de9868
mattcaswell <matthewcaswell253@Gmail.com>
2017-02-24 11:11:03 -0800

    add succesful second test file

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -3,3 +3,29 @@ using System.Collections.Generic;
 using System;
 using System.Data;
 using System.Data.SqlClient;
+
+namespace HairSalon
+{
+    public class StylistTest : IDisposable
+    {
+      public StylistTest()
+      {
+        DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+      }
+
+      [Fact]
+      public void StylistTest_DatabaseEmptyAtFirst_True()
+      {
+        //Arrange, Act
+        int result = Stylist.GetAll().Count;
+
+        //Assert
+        Assert.Equal(0, result);
+      }
+
+      public void Dispose()
+      {
+        Stylist.DeleteAll();
+      }
+    }
+}
